f05d06a0193dea8dadaaedd79caf288941c83657
Alex Tung <atung323@gmail.com>
2021-02-23 21:31:45 -0800

    logs and requirements.txt

--- launch-sgl-container.sh
+++ launch-sgl-container.sh
@@ -15,6 +15,10 @@ K8S_TIMEOUT_SECONDS=(3600*24) # note: be careful with this! 48h timeout should b
 SPAWN_INTERACTIVE_SHELL=YES
 
 # Sets up proxy URL for Jupyter notebook inside 
+#K8S_INIT="/opt/k8s-support/bin/pause"
+#K8S_ENTRYPOINT_ARGS=()
+#SPAWN_INTERACTIVE_SHELL=NO
+
 PROXY_ENABLED=YES
 #K8S_ENTRYPOINT="/bin/sleep"
 #K8S_ENTRYPOINT_ARGS_EXPANDED='"8640000"'
